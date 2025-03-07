44deaa0b56597d5899ab8c3192f0aa923b75c03c
janchik7 <danil.yuzhaninoff@gmail.com>
2025-03-07 10:58:11 +0500

    Add files via upload

---
+++ LAB4/LAB4/Clln.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Clln<T> {
+
+    public static List<T> intersect(List<T> L1, List<T> L2)
+    {
+        List<T> res = new List<T>();
+        foreach (T val in L1)
+            if (hasVal(L2, val) && !hasVal(res, val))
+                res.Add(val);
+        return res;
+    }
+
+    public static LinkedList<T> addReverse(LinkedList<T> LL)
+    {
+        LinkedList<T> res = new LinkedList<T>(LL);
+        for (LinkedListNode<T> i = LL.Last; i != null; i = i.Previous)
+            res.AddLast(i.Value);
+        return res;
+    }
+
+    private static bool hasVal<T>(List<T> L, T v)
+    {
+        foreach (T val in L)
+            if (v.Equals(val))
+                return true;
+        return false;
+    }
+
+    public static void checkAtten(HashSet<string> mall, HashSet<string>[] stud)
+    {
+        bool valid;
+
+        foreach (string ml in mall)
+        {
+            valid = true;
+            foreach (HashSet<string> hs in stud)
+            {
+                if (!hs.Contains(ml))
+                {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid)
+                Console.WriteLine($"В ТРЦ {ml} ходили все.");
+        }
+
+        foreach (string ml in mall)
+        {
+            valid = false;
+            foreach (HashSet<string> hs in stud)
+            {
+                if (hs.Contains(ml))
+                {
+                    valid = true;
+                    break;
+                }
+            }
+            if (valid)
+                Console.WriteLine($"В ТРЦ {ml} ходил хотя-бы один студент.");
+            if (!valid)
+                Console.WriteLine($"В ТРЦ {ml} никто не ходил.");
+        }
+    }
+
+    static public void checkSym(string e)
+    {
+        // Создаём файл в текущей директории
+        string filePath = e;
+
+        Console.WriteLine("Введите текст (для завершения нажмите Enter дважды):");
+
+        // Создаём файл и открываем его для записи
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            string line;
+            while ((line = Console.ReadLine()) != string.Empty)
+            {
+                writer.WriteLine(line);
+            }
+        }
+
+        // Читаем текст из файла для подсчёта букв
+        string text = File.ReadAllText(filePath);
+
+        // Подсчёт уникальных букв
+        HashSet<char> uniqueLetters = new HashSet<char>(
+                text.Where(c => (c >= 'А' && c <= 'Я') || (c >= 'а' && c <= 'я') || c == 'Ё' || c == 'ё')
+            );
+
+
+        Console.WriteLine("\nРезультаты:");
+        Console.WriteLine($"Количество уникальных букв: {uniqueLetters.Count}");
+        Console.WriteLine("Уникальные буквы: " + string.Join(", ", uniqueLetters.OrderBy(c => c)));
+
+    }
+
+}
+
---
+++ LAB4/LAB4/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+internal class Program {
+    private static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Задание 1:");
+        Console.WriteLine("Введите списки L1, L2, и будет выведено их пересечение L.");
+        List<int> L1 = new List<int>(securedList(securedInt("Введите размер списка L1:", true)));
+        List<int> L2 = new List<int>(securedList(securedInt("Введите размер списка L2:", true)));
+        Console.WriteLine("Список L:");
+        foreach (int v in Clln<int>.intersect(L1, L2))
+            Console.Write($"{v} ");
+        Console.WriteLine("\n\n\n");
+
+
+        Console.WriteLine("Задание 2:");
+        Console.WriteLine("\nВведите список L, и в конец его будут добавлены его элементы в обратном порядке.");
+        LinkedList<int> LL = new LinkedList<int>(securedList(securedInt("Введите размер списка LL:", true)));
+        Console.WriteLine("Список LL с элементами в обратном порядке в конце:");
+        foreach (int v in Clln<int>.addReverse(LL))
+            Console.Write($"{v} ");
+        Console.WriteLine("\n\n\n");
+
+
+        Console.WriteLine("Задание 3:");
+        Console.WriteLine("\nПосещение ТРЦ:\nВведите все ТРЦ через пробел:");
+        HashSet<string> trz = [.. Console.ReadLine().Split(' ')];
+        int size = securedInt("Введите количество студентов", true);
+        HashSet<string>[] studs = new HashSet<string>[size];
+        for (int i = 0; i < size; ++i)
+        {
+            Console.WriteLine($"Студент номер {i + 1} посещал:");
+            studs[i] = [.. Console.ReadLine().Split(' ')];
+        }
+        Clln<int>.checkAtten(trz, studs);
+        Console.WriteLine("\n\n\n");
+
+        Console.WriteLine("Задание 4:");
+        Clln<int>.checkSym("inp.txt");
+
+    }
+
+
+
+    private static int securedInt(string mes, bool isZ)
+    {
+        int res;
+
+        do
+        {
+            Console.WriteLine(mes);
+        } while (!(int.TryParse(Console.ReadLine(), out res)) || (isZ && res <= 0)); // try convert input to int as res
+
+        return res;
+    }
+
+
+    private static int[] securedList(int size, string mes = "Введите список через пробел:")
+    {
+        int[] res = new int[size];
+        int i = 0;
+
+        do
+        {
+            Console.WriteLine(mes);
+
+            foreach (string val in Console.ReadLine().Split(' '))
+            { // reading string and splitting it by spaces
+                if (i >= size || !int.TryParse(val, out res[i++]))
+                { // if val is valid as int; write it in res[i] if valid, or reset
+                    i = 0; break;
+                }
+            }
+        } while (!(i == size)); // check if is valid
+
+        return res;
+    }
+
+}
