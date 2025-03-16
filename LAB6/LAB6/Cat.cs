02e6f2d6c6315864bece1d3084d588cefcaf0309
janchik7 <danil.yuzhaninoff@gmail.com>
2025-03-17 01:34:16 +0500

    Add files via upload

---
+++ LAB6/LAB6/Cat.cs
@@ -0,0 +1,19 @@
+﻿class Cat
+{
+    private readonly string name;
+
+    public Cat(string _name)
+    {
+        name = _name;
+    }
+
+    public void meow(int amount = 1)
+    {
+        if (amount < 1)
+            return;
+        Console.Write($"{name}: ");
+        for (int i = 0; i < amount - 1; ++i)
+            Console.Write("мяу-");
+        Console.WriteLine("мяу!");
+    }
+}
---
+++ LAB6/LAB6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Formats.Asn1;
+using System.Reflection.Metadata;
+using System.Security;
+
+internal class Programm
+{
+    private static void Main(string[] args)
+    {
+        Cat barsik = new("Барсик");
+        barsik.meow();
+        barsik.meow(3);
+    }
+}
